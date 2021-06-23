cffe1aae0fb5bd307654b2c06c72806703a21822
xiaoqie <xiaoqie108@gmail.com>
2021-06-23 10:06:56 +0800

    commit

--- streams/encode.py
+++ streams/encode.py
@@ -18,28 +18,18 @@ if videos.items():
     if video_list:
         video = video_list[0]
 
-        if os.name == 'posix':
-            with open(f'{video.path_without_ext}.ass') as inp:
-                with open(f'temp.ass', 'w') as out:
-                    out.write(inp.read())
-        else:
-            with open(f'{video.path_without_ext}.ass', encoding='utf-8') as inp:
-                with open(f'temp.ass', 'w', encoding='utf-8') as out:
-                    out.write(inp.read())
-
         resolution = video.resolution
         if min(resolution) >= 1080:
             bitrate = '3000k'
         else:
             bitrate = '2000k'
-        cmd = f'{ffmpeg} -analyzeduration 2147483647 -probesize 2147483647 -y -f live_flv -i "{video.path_without_ext}.flv" -vf "subtitles=temp.ass" -c:v libx264 -preset superfast -b:v {bitrate} -c:a aac -b:a 128k -r 30 -max_muxing_queue_size 20000 "{video.path_without_ext}.mp4"'
+        cmd = f'{ffmpeg} -analyzeduration 2147483647 -probesize 2147483647 -y -f flv -i "{video.path_without_ext}.flv" -vf "subtitles={video.path_without_ext}.ass" -c:v libx264 -preset superfast -b:v {bitrate} -c:a aac -b:a 128k -r 30 -max_muxing_queue_size 20000 "{video.path_without_ext}.mp4"'
         print(cmd)
         ret_code = os.system(cmd)
         if ret_code != 0:
             move_to_trash(f'{video.path_without_ext}.mp4')
             raise Exception(f"{cmd} returns non-zero status {ret_code}.")
-        if resolution[1] < resolution[0]:
-            move_to_trash(f'{video.path_without_ext}.flv')
-            move_to_trash(f'{video.path_without_ext}.ass')
-            empty_trash()
+        move_to_trash(f'{video.path_without_ext}.flv')
+        move_to_trash(f'{video.path_without_ext}.ass')
+        empty_trash()
 
